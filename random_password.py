e0ff5618be1fb38a67c38d7aff0800adaf5d9b22
Bosco So <bso@alum.mit.edu.nospam>
2019-01-28 17:47:58 -0800

    finished adding of new client
    - ran "black" formatter over all code

--- random_password.py
+++ random_password.py
@@ -4,18 +4,18 @@ from random import choice
 # removed ambiguous symbol: |, \, ;
 # removed comma as it screws with CSV
 charsets = [
-    'abcdefghjkmnpqrstuvwxyz',
-    'ABCDEFGHJKMNPQRSTUVWXYZ',
-    '23456789',
-    '^$%&/()=?{[]}+~#-_.:;<>',
+    "abcdefghjkmnpqrstuvwxyz",
+    "ABCDEFGHJKMNPQRSTUVWXYZ",
+    "23456789",
+    "^$%&/()=?{[]}+~#-_.:;<>",
 ]
 
 
 def make_password(length=12):
-    all_chars = ''.join(charsets)
+    all_chars = "".join(charsets)
     pwd = []
     while len(pwd) < length:
         rand_char = choice(all_chars)
         if rand_char not in pwd:
             pwd.append(rand_char)
-    return ''.join(pwd)
+    return "".join(pwd)
--- whcms_csv_importer.py
+++ whcms_csv_importer.py
@@ -1,29 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from us_states_abbreviations import STATES
 
 import csv
 import random_password
 
-ACCOUNT_NO_KEY = 'Account No.'
-PASSWORD_KEY = 'Password'
+ACCOUNT_NO_KEY = "Account No."
+PASSWORD_KEY = "Password"
 
-CSV_HEADER = [
-    ACCOUNT_NO_KEY,
-    'First Name',
-    'Last Name',
-    'Company',
-    'Email',
-    'Address 1',
-    'City',
-    'State/ Region',
-    'Post Code',
-    'Phone Number',
-    'URL ',
-    'Wyoming Network Client (check box)',
-    'CSS #',
-]
+# Maps header in CSV file to argument name in
+# `enter_new_client_info`. Allows passing row from CSV to
+# `enter_new_client_info` as a dict so it can be out of order.
+#
+CSV_HEADER = {
+    ACCOUNT_NO_KEY: "account_no",
+    "First Name": "first_name",
+    "Last Name": "last_name",
+    "Company": "company_name",
+    "Email": "email",
+    "Address 1": "address",
+    "City": "city",
+    "State/ Region": "state",
+    "Post Code": "zip",
+    "Phone Number": "phone",
+    "URL ": "url",
+    "Wyoming Network Client (check box)": "is_network_client",
+    "CSS #": "css_no",
+}
 
 
 def blacklist_key(mapping):
@@ -47,22 +53,33 @@ def import_csv(im, csv_fname, black_list={}):
     """
     dicts = read_csv(csv_fname)
     count = 0
-    for mapping in dicts:
-        collision_key = blacklist_key(mapping)
-        if collision_key in black_list: continue
+    for row_dict in dicts:
+        collision_key = blacklist_key(row_dict)
+        if collision_key in black_list:
+            continue
 
-        args = [mapping[_key] for _key in CSV_HEADER]
-        new_password = im.enter_new_client_info(*args)
+        kw_args = {CSV_HEADER[_key]: row_dict[_key] for _key in CSV_HEADER}
+        new_password = im.enter_new_client_info(**kw_args)
 
-        mapping[PASSWORD_KEY] = new_password
-        black_list[collision_key] = mapping
+        # submit new client info
+        btn_submit = im.driver.find_element_by_css_selector('input[value="Add Client"]')
+        btn_submit.submit()
+
+        # verify we're in client profile page
+        page_title = WebDriverWait(im.driver, 20).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, "h1"), "Client Profile")
+        )
+        im.open_new_client_page()
+
+        row_dict[PASSWORD_KEY] = new_password
+        black_list[collision_key] = row_dict
 
     return black_list
 
 
 def read_csv(fname):
     matrix = []
-    with open(fname, mode='r') as csv_file:
+    with open(fname, mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             matrix.append(row)
@@ -70,7 +87,6 @@ def read_csv(fname):
 
 
 class WhcmsCsvImporter(object):
-
     def __init__(self):
         self.driver = webdriver.Firefox()  # type: WebDriver
         self.driver.implicitly_wait(20)
@@ -93,24 +109,26 @@ class WhcmsCsvImporter(object):
         elem_pass.send_keys(password)
 
         # submit
-        elem_submit = self.driver.find_element_by_css_selector('input.btn[type="submit"')
+        elem_submit = self.driver.find_element_by_css_selector(
+            'input.btn[type="submit"'
+        )
         elem_submit.submit()
 
         # successful login will implicitly wait and find logout link
-        self.driver.find_element_by_id('logout')
-        assert self.driver.title == u'WHMCS - Dashboard'
+        self.driver.find_element_by_id("logout")
+        assert self.driver.title == u"WHMCS - Dashboard"
 
     def logout(self):
-        elem_logout = self.driver.find_element_by_id('logout')
+        elem_logout = self.driver.find_element_by_id("logout")
         elem_logout.click()
 
         # implicitly wait for alert to appear
-        elem_alert = self.driver.find_element_by_id('alertLoginSuccess')
-        assert elem_alert.text == u'You have been successfully logged out.'
+        elem_alert = self.driver.find_element_by_id("alertLoginSuccess")
+        assert elem_alert.text == u"You have been successfully logged out."
 
-    def new_client(self):
-        menu = self.driver.find_element_by_id('Menu-Clients')
-        hidden_submenu = self.driver.find_element_by_id('Menu-Clients-Add_New_Client')
+    def open_new_client_page(self):
+        menu = self.driver.find_element_by_id("Menu-Clients")
+        hidden_submenu = self.driver.find_element_by_id("Menu-Clients-Add_New_Client")
 
         actions = webdriver.ActionChains(self.driver)
         actions.move_to_element(menu)
@@ -119,40 +137,53 @@ class WhcmsCsvImporter(object):
         actions.perform()
 
         # implicitly wait for firstname to appear
-        self.driver.find_element_by_name('firstname')
+        self.driver.find_element_by_name("firstname")
 
-    def enter_new_client_info(self, account_no,
-                              first_name, last_name, company_name, email,
-                              address, city, state, zip,
-                              phone,
-                              url, is_network_client, css_no):
-        self._fill_text_input('firstname', first_name)
-        self._fill_text_input('lastname', last_name)
-        self._fill_text_input('companyname', company_name)
-        self._fill_text_input('email', email)
+    def enter_new_client_info(
+        self,
+        account_no,
+        first_name,
+        last_name,
+        company_name,
+        email,
+        address,
+        city,
+        state,
+        zip,
+        phone,
+        url,
+        is_network_client,
+        css_no,
+    ):
+        self._fill_text_input("firstname", first_name)
+        self._fill_text_input("lastname", last_name)
+        self._fill_text_input("companyname", company_name)
+        self._fill_text_input("email", email)
 
         password = random_password.make_password()
-        self._fill_text_input('password', password)
+        self._fill_text_input("password", password)
 
-        self._fill_text_input('address1', address)
-        self._fill_text_input('city', city)
-        self._select_option('state', state)
-        self._fill_text_input('postcode', zip)
-        self._fill_text_input('phonenumber', phone)
+        self._fill_text_input("address1", address)
+        self._fill_text_input("city", city)
+        self._select_option("state", state)
+        self._fill_text_input("postcode", zip)
+        self._fill_text_input("phonenumber", phone)
 
-        self._fill_text_input('customfield[5]', url)
-        self._check_radio_button('customfield[16]', is_network_client == 'Yes')
-        self._fill_text_input('customfield[17]', css_no)
+        self._fill_text_input("customfield[5]", url)
+        self._check_radio_button("customfield[16]", is_network_client == "Yes")
+        self._fill_text_input("customfield[17]", css_no)
 
-        self._fill_text_input('notes', 'Account No: {}'.format(account_no))
+        self._fill_text_input("notes", "Account No: {}".format(account_no))
         return password
 
     def _check_radio_button(self, button_name, should_check):
         cb = self.driver.find_element_by_name(button_name)
         if cb.is_selected():
-            if not should_check: cb.click()
+            if not should_check:
+                cb.click()
         else:
-            if should_check: cb.click()
+            if should_check:
+                cb.click()
 
     def _fill_text_input(self, field_name, field_value):
         if field_value:
@@ -179,7 +210,7 @@ if __name__ == "__main__":
 
     importer = WhcmsCsvImporter()
     importer.login(url, username, password)
-    importer.new_client()
+    importer.open_new_client_page()
     black_list = import_csv(importer, csv_fname)
     importer.logout()
     importer.cleanup()
